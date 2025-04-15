c91e5b4372390a56ab5a47caaafc678b5a345662
Steven <lijt_001@sina.com>
2025-04-16 04:09:28 +0800

    在ASP.Net项目中初步实现了看板

---
+++ TanpopoWeb/Data/ProductionItem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace TanpopoWeb.Data
+{
+    public class ProductionItem
+    {
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Machine { get; set; }
+        public string Order { get; set; }
+        public int? Quantity { get; set; }
+        public string Type { get; set; }
+    }
+}
